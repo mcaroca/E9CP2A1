cf722f9cc3f5fb00aebe918909c80804511fa4bb
Mauricio Caroca <maucaroca@gmail.com>
2018-08-01 21:31:00 -0400

    Bad solution

--- 2.rb
+++ 2.rb
@@ -4,7 +4,17 @@ class Course
   attr_reader :name
   def initialize(name, *dates)
     @name = name
-    @dates = dates.map{ |e| Date.parse(e) }
+    @dates = dates
+  end
+
+  def after date_for_compare
+    raise ArgumentError.new('argument is not a date') if date_for_compare.class != Date 
+    @dates.select{ |date| Date.parse(date) < date_for_compare }
+  end
+
+  def before date_for_compare
+    raise ArgumentError.new('argument is not a date') if date_for_compare.class != Date 
+    @dates.select{ |date| Date.parse(date) > date_for_compare }
   end
 
 end
@@ -17,4 +27,8 @@ courses = []
 data.each do |info|
   ls = info.split(', ')
   courses << Course.new(*ls)
+end
+
+courses.each do |course|
+  puts "Cursos previos: #{course.name} #{course.after(Date.today)} \nCursos posteriores: #{course.name} #{course.before(Date.today)}"
 end
